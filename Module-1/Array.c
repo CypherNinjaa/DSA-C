0495515f95db454b181074dd00d0a9ad3eb280c5
vikash <vikashkelly@gmail.com>
2025-02-28 10:30:18 +0530

    array

---
+++ Module-1/Array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int arr[4]={1,2,3,4};
+  printf("1st element : [%d]\n",arr[0]);
+  printf("2nd element : [%d]\n",arr[1]);
+  printf("3rd element : [%d]\n",arr[2]);
+  printf("4th element : [%d]\n",arr[3]);
+    return 0;
+}

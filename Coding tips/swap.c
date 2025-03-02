3e9f49c356e10e3f450cd43d636cb165bc7aab3b
vikash <vikashkelly@gmail.com>
2025-03-02 08:53:02 +0530

    deleted

--- Coding tips/swap.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a=10,b=20;
-    a=a+b-(b=a);
-    printf("a=%d and b=%d\n",a,b);
-    
-    return 0;
-}
--- trash/1d_Array.c
+++ trash/1d_Array.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-int arrayinput(int*);
-int arrayprint(int*);
-int main(){
+void arrayinput(int *);
+void arrayprint(int *);
+int main()
+{
     int arr[5];
-    int* ptr=arr;
+    int *ptr = arr;
     arrayinput(ptr);
     arrayprint(ptr);
     return 0;
 }
-int arrayinput(int* ptr){
+void arrayinput(int *ptr)
+{
     for (int i = 0; i < 5; i++)
     {
-        printf("Enter %d term : ",i);
-        scanf("%d",ptr);
+        printf("Enter %d term : ", i);
+        scanf("%d", ptr);
         ptr++;
     }
-    return 0;
 }
-int arrayprint(int* ptr){
-    for (int i = 0; i <5; i++)
+void arrayprint(int *ptr)
+{
+    for (int i = 0; i < 5; i++)
     {
-       printf("%d term = %d\n",i,*ptr);
-       ptr++;
+        printf("%d term = %d\n", i, *ptr);
+        ptr++;
     }
-    return 0;
 }

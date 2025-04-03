1feb649d06f105a9e74c6ecf8440c648b67d9c61
vikash <vikashkelly@gmail.com>
2025-04-03 09:51:27 +0530

    #

--- linked list/likedlist.c
+++ linked list/likedlist.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node{
+struct node
+{
     int data;
-    struct node* next;
+    struct node *next;
 };
-void traverse(struct node* ptr){
-    while(ptr!=NULL){
+void traverse(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
         printf("Element : %d\n", ptr->data);
-        ptr=ptr->next;
+        ptr = ptr->next;
     }
 }
-int main(){
-    struct node* head;
-    struct node* second;
-    struct node* third;
-    struct node* fourth;
-    //allocate memory
-    head = (struct node *) malloc(sizeof(struct node));
-    second = (struct node *) malloc(sizeof(struct node));
-    third = (struct node *) malloc(sizeof(struct node));
-    fourth = (struct node *) malloc(sizeof(struct node));
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+    // allocate memory
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
 
-    //link first and second
-    head-> data=7;
-    head-> next=second;
+    // link first and second
+    head->data = 7;
+    head->next = second;
 
-    //link second and third
-    second-> data=44;
-    second-> next=third;
+    // link second and third
+    second->data = 44;
+    second->next = third;
 
-    //link second and third
-    third-> data=44;
-    third-> next=fourth;
+    // link second and third
+    third->data = 44;
+    third->next = fourth;
 
-    //link last
-    fourth-> data=66;
-    fourth-> next=NULL;
+    // link last
+    fourth->data = 66;
+    fourth->next = NULL;
 
-        traverse(head);
+    traverse(head);
     return 0;
 }
